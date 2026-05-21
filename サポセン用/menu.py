04169f25ef7a712cc4cad08d3ac72abbfca0e9cc
ecsvai <xyunhao909@gmail.com>
2026-05-21 15:58:45 +0900

    2

--- サポセン用/menu.py
+++ サポセン用/menu.py
@@ -1,11 +1,11 @@
 import streamlit as st
+import pathlib as pl
 
-
-
+base_dir = pl.Path(__file__).resolve().parent
 
 st.title('日本外国人統計データ')
 st.write('総数＝中長期在留者及び特別永住者（短期滞在者が含まない）')
-st.image('group_young_world.png')
+st.image(base_dir/'data'/'group_young_world.png')
 st.balloons()
 
 
