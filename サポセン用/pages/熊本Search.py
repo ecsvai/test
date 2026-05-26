0a488f8d17785e35b8f50295356c92a2fced955d
ecsvai <xyunhao909@gmail.com>
2026-05-26 10:32:24 +0900

    update5/26

--- サポセン用/pages/熊本Search.py
+++ サポセン用/pages/熊本Search.py
@@ -2,17 +2,49 @@ import pandas as pd
 import streamlit as st
 import pathlib as pl
 
-BASE_DIR = pl.Path(__file__).resolve().parent.parent
-data_path = BASE_DIR / 'data'/'25-06kumamoto.csv'
 
-st.write('＊＊選択肢「-」＝「わからない」＊＊')
-st.write('熊本市以外の市町村は「性別」・「年齢」の統計データがありません')
+BASE_DIR = pl.Path(__file__).resolve().parent.parent
+data_2506 = BASE_DIR / 'data'/'25-06kumamoto.csv'
+data_2406 = BASE_DIR / 'data'/'24-06kumamoto.csv'
+data_2306_main = BASE_DIR / 'data'/'23-06kumamoto_main.csv'
+data_2206_main = BASE_DIR / 'data'/'22-06kumamoto_main.csv'
+data_2106_main = BASE_DIR / 'data'/'21-06kumamoto_main.csv'
+
+
+st.caption('＊＊選択肢「-」＝「わからない」＊＊')
+st.caption('2025のデータに熊本市以外の市町区村は「性別」・「年齢」の統計データがありません')
+st.caption('2024以前のデータは「市町区村」・「在留資格」・「国籍・地域」のクロスサーチができません')
+
+years = st.selectbox('Choose Year',
+                     ['-','2025','2024','2023','2022','2021'])
+
+year_list = {
+    '2025':data_2506,
+    '2024':data_2406,
+    '2023':data_2306_main,
+    '2022':data_2206_main,
+    '2021':data_2106_main
+}
 
 @st.cache_data
-def load_data():
-    return pd.read_csv(data_path, header=0)
+def load_data(years):
+    if years in year_list:
+        return pd.read_csv(year_list[years])
 
-full_table = load_data()
+
+if years == '-':
+    full_table = None
+    st.stop()
+
+else:
+    full_table = load_data(years)
+    cols = full_table.columns.drop("在留外国人数")
+
+full_table[cols] = full_table[cols].ffill()
+
+if "市区町村コード" in full_table.columns:
+    full_table = full_table.drop(columns = '市区町村コード')
+
 
 keys = ['city','qual','sex','age5sai','pref','nat']
 def reset():
@@ -20,34 +52,39 @@ def reset():
         st.session_state[key] = '全部'
 st.button('reset',on_click=reset)
 
-full_table[[
-    '都道府県','市区町村','年齢（５歳階級）','性別','国籍・地域']] = (
-    full_table[[
-    '都道府県','市区町村','年齢（５歳階級）','性別','国籍・地域']].ffill())
-
 result = full_table
 
 filter_text = ''
 
+if '市区町村' in result.columns:
+    city = st.selectbox(
+        '市区町村',
+        ['全部']+list(full_table['市区町村'].unique()),
+        key = 'city'
+    )
+    if city != '全部':
+        result = result[result['市区町村'] == city]
+        filter_text = '\n市区町村=' + city
 
+if '年齢（５歳階級）' in result.columns:
+    age5sai = st.selectbox(
+        '年齢（５歳階級）',
+        ['全部']+list(full_table['年齢（５歳階級）'].unique()),
+        key = 'age5sai'
+    )
+    if age5sai != '全部':
+        result = result[result['年齢（５歳階級）']==age5sai]
+        filter_text += '\n年齢（５歳階級）=' + age5sai
 
-city = st.selectbox(
-    '市区町村',
-    ['全部']+list(full_table['市区町村'].unique()),
-    key = 'city'
-)
-
-age5sai = st.selectbox(
-    '年齢（５歳階級）',
-    ['全部']+list(full_table['年齢（５歳階級）'].unique()),
-    key = 'age5sai'
-)
-
-sex = st.selectbox(
-    '性別',
-    ['全部']+list(full_table['性別'].unique()),
-    key = 'sex'
-)
+if '性別' in result.columns:
+    sex = st.selectbox(
+        '性別',
+        ['全部']+list(full_table['性別'].unique()),
+        key = 'sex'
+    )
+    if sex != '全部':
+        result = result[result['性別']==sex]
+        filter_text += '\n性別=' + sex
 
 nat = st.selectbox(
     "国籍・地域",
@@ -61,17 +98,8 @@ qual = st.selectbox(
     key = 'qual'
 )
 
-if city != '全部':
-    result = result[result['市区町村']==city]
-    filter_text = '\n市区町村=' + city
 
-if age5sai != '全部':
-    result = result[result['年齢（５歳階級）']==age5sai]
-    filter_text += '\n年齢（５歳階級）=' + age5sai
 
-if sex != '全部':
-    result = result[result['性別']==sex]
-    filter_text += '\n性別=' + sex
 
 if nat != '全部':
     result = result[result['国籍・地域'] == nat]
@@ -82,20 +110,20 @@ if qual != '全部':
     filter_text += '\n在留資格=' + qual
 
 
-result['合計 / 在留外国人数'] = pd.to_numeric(
-    result['合計 / 在留外国人数'].astype(str).str.replace(',', ''),
+result['在留外国人数'] = pd.to_numeric(
+    result['在留外国人数'].astype(str).str.replace(',', ''),
     errors='coerce'
 )
 
 
 st.divider()
-st.header('総数＝'+str(result['合計 / 在留外国人数'].sum()))
+st.header('総数＝'+str(result['在留外国人数'].sum()))
 st.divider()
 st.code('filter:' + filter_text)
 
+for x in result.columns.drop(['在留外国人数','都道府県']):
+    st.badge('順位表（'+x+')')
+    st.dataframe(result.groupby(x).sum()['在留外国人数'].sort_values(ascending=False))
 
-st.dataframe(result.groupby('在留資格').sum()['合計 / 在留外国人数'].sort_values(ascending=False))
 
-
-st.dataframe(result.groupby('市区町村').sum()['合計 / 在留外国人数'].sort_values(ascending=False))
 
