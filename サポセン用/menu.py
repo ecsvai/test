b724b61940b68193fe54701acda376f51e78299d
ecsvai <xyunhao909@gmail.com>
2026-06-16 17:05:02 +0900

    Update06/16 v3

--- サポセン用/menu.py
+++ サポセン用/menu.py
@@ -5,6 +5,8 @@ base_dir = pl.Path(__file__).resolve().parent
 
 st.title('日本外国人統計データ')
 st.write('総数＝中長期在留者及び特別永住者（短期滞在者が含まない）')
+st.write('***毎年６月のデータを参照***')
+st.divider()
 st.image(base_dir/'data'/'group_young_world.png')
 st.balloons()
 
